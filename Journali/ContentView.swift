2b03ebe9d4ff553a304f84e5e1e77afe32b4ad44
Tahani <Tahani.14192@gmail.com>
2024-11-07 11:29:53 +0300

    Initial Commit

---
+++ Journali/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Journali
+//
+//  Created by Tahani Mohammed Alhawsawi on 05/05/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Tahani Mohammed Alhawsawi on 05/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
